49a2005eb50ab2c0589edecbec1ee293e70f19c0
jiancheng <1729303158@qq.com>
2023-03-27 23:18:27 +0800

    doc: Remove Chinese

--- pon/cli/main.py
+++ pon/cli/main.py
@@ -1,10 +1,9 @@
 import os
-from pathlib import Path
-from typing import Tuple, Union, Optional
-import click
-from loguru import logger
-import eventlet
 import time
+from pathlib import Path
+from typing import Tuple, Optional
+import click
+import eventlet
 
 
 def run_forever():
@@ -21,7 +20,7 @@ def pon_cli():
 @click.argument('services', nargs=-1, required=True)
 @click.option('--config', help='config file path')
 def run(services: Tuple[str], config: Optional[str] = None):
-    """ 启动 pon 服务 """
+    """ run pon services """
     config_filepath: Path = Path(os.getcwd())/config
     from pon.events import EventletEventRunner
     from pon.web import EventletAPIRunner
@@ -29,14 +28,14 @@ def run(services: Tuple[str], config: Optional[str] = None):
     eventlet.spawn_n(EventletEventRunner().run, services, config_filepath)
     # eventlet.spawn_n(EventletAPIRunner().run, services, config_filepath)
 
-    gt = eventlet.spawn(run_forever) 
+    gt = eventlet.spawn(run_forever)
     gt.wait()
 
 
 @pon_cli.command()
 @click.option('--config', help='config file path')
 def shell(file_path: str = None):
-    """ shell 交互环境 """
+    """ shell interactive environment """
     pass
 
 
